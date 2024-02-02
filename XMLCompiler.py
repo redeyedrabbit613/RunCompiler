00b43fa07fb8911d83e984fd4290bc110ddf80be
redeyedrabbit613 <115123952+redeyedrabbit613@users.noreply.github.com>
2024-02-01 20:52:21 -0800

    Add files via upload

--- XMLCompiler.py
+++ XMLCompiler.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 28 16:43:19 2023
+Created on Thursday February 2 20:51:19 2024
 
 @author: John Wang
 """
 import glob as gl
 import xml.etree.ElementTree as ET
+import openpyxl as op
+# Create a new workbook
+workbook = op.Workbook()
+# Select the active worksheet
+worksheet = workbook.active
 
-#define the pattern to search for .txt files
-pattern = '\\OMNIOME.LOCAL\Shares\Shared\systems integration\__SystemsIntegration_Team_Members\_JW\Onso\OCG\Flow Cell Cluster Density Investigation\OCGs\OCG1\10-1.24 run folder\20??????_FC???????-BCC_OCG01_???\Logs\FC???????-BCC_metadata.xml'
 
-#use glob to find the files matching the pattern
+# Define the pattern to search for .XML files
+pattern = ''
+
+# Use glob to find the files matching the pattern
 xml_files = gl.glob(pattern)
 
 # Function to read attribute for a specific value
-def read_attribute_for_value(xml_file, element_tag, attribute_name, target_value):
+def read_attribute_for_value(xml_file, element_tag, attribute_name, attribute_name2, target_value):
     tree = ET.parse(xml_file)
     root = tree.getroot()
     
@@ -22,22 +28,22 @@ def read_attribute_for_value(xml_file, element_tag, attribute_name, target_value
     for elem in root.iter(element_tag):
         # Check if the element has the target value for the specified attribute
         if elem.get(attribute_name) == target_value:
-            return elem.get(attribute_name)
-    
-    return None  # If target value not found
-
-# Specify the element tag, attribute name, and target value
+            # Get the value of attribute_name2
+            run_folder = root.attrib[attribute_name2]
+            # Write the value to the next empty cell in the first column of the worksheet
+            next_cell = worksheet.cell(row=worksheet.max_row + 1, column=1)
+            next_cell.value = run_folder
+            # Save the workbook to a file
+            workbook.save('/path/to/directory/SOPRuns.xlsx')
+            
+# Specify the element tag, attribute name, attribute name 2, and target value
 element_tag = 'ClusterRunMetadata'
 attribute_name = 'Fragment_FilePath'
+attribute_name2 = 'Run_Folder_Name'
 target_value = 'STANDARD'
 
-# Iterate over XML files and read attribute for the specific value
-for xml_file in xml_files:
-    attribute_value = read_attribute_for_value(xml_file, element_tag, attribute_name, target_value)
-    if attribute_value:
-        print(f"In {xml_file}, attribute '{attribute_name}' for '{element_tag}' with value '{target_value}' is '{attribute_value}'")
-    else:
-        print(f"In {xml_file}, attribute '{attribute_name}' for '{element_tag}' with value '{target_value}' not found")
+
+
 
         
 """
