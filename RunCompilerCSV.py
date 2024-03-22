e149c49f053bebd86ddeb2ee4f155fed52c41214
Deonardo Licaprio <115123952+redeyedrabbit613@users.noreply.github.com>
2024-03-22 09:59:02 -0700

    Add files via upload

--- RunCompilerCSV.py
+++ RunCompilerCSV.py
@@ -5,20 +5,20 @@ Created on Sat Oct 28 16:43:19 2023
 @author: John Wang
 """
 #'glob' is a module used for searching and manipulating files and directories based on wildcard patterns.
-import glob as g
+import glob as gl
 import pandas as pd
 
-#Define the pattern to search for .txt files
-pattern = 'S:/sd.nanofluidics.com/rddata/OnsoData/202310??_?????????-?CC_OSQ?_??/AF_IMAGES/?????????-?CC_af_log.csv'
+#Define the pattern to search for .csv files
+pattern = '//sd.nanofluidics.com/rddata/OnsoData/202310??_?????????-?CC_OSQ007_??/AF_IMAGES/?????????-?CC_af_log.csv'
 
 #Use glob to find the files matching the pattern
-file_list = g.glob(pattern)
+file_list = gl.glob(pattern)
 CompiledAFLogs = None
 dataframes = []
 
 #Use enumerate() to keep track of the index
 for index, file in enumerate(file_list):
-    file_num = f"AFLog{index + 1}"
+    file_num = f"AFLog{index + 1}" #Assigns the variable with the string with the indexed number attached
     df = pd.read_csv(file)  # Read the csv file and store it in df
     df['AFLog'] = file_num  # Add the AFLog coloumn
     dataframes.append(df) # Append the DataFrame to the list
@@ -27,4 +27,4 @@ CompiledAFLogs = pd.concat(dataframes, ignore_index=True)
 #Turns all values in a specific coloumn into an absolute value and reassigns them to a new coloumn.
 CompiledAFLogs['AbsMoveMm'] = CompiledAFLogs[' MoveMm'].abs()
 #Writes CompiledAFLogs data frames' to a csv file in the designated path. 
-CompiledAFLogs.to_csv('#Desired filepath') 
+CompiledAFLogs.to_csv('C:/Users/jowang/OneDrive - Pacific Biosciences of California\Desktop/Python/RunComplier/Final/CompiledAFLogs.csv') 
--- XMLCompiler.py
+++ XMLCompiler.py
@@ -1,54 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thursday February 2 20:51:19 2024
+Created on Fri Feb  2 08:42:04 2024
 
-@author: John Wang
+@author: jowang
 """
+
 import glob as gl
 import xml.etree.ElementTree as ET
 import openpyxl as op
-# Create a new workbook
-workbook = op.Workbook()
-# Select the active worksheet
-worksheet = workbook.active
 
-
-# Define the pattern to search for .XML files
-pattern = ''
-
-# Use glob to find the files matching the pattern
-xml_files = gl.glob(pattern)
-
-# Function to read attribute for a specific value
-def read_attribute_for_value(xml_file, element_tag, attribute_name, attribute_name2, target_value):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    
-    # Iterate over elements with the specified tag
-    for elem in root.iter(element_tag):
-        # Check if the element has the target value for the specified attribute
-        if elem.get(attribute_name) == target_value:
-            # Get the value of attribute_name2
-            run_folder = root.attrib[attribute_name2]
-            # Write the value to the next empty cell in the first column of the worksheet
-            next_cell = worksheet.cell(row=worksheet.max_row + 1, column=1)
-            next_cell.value = run_folder
-            # Save the workbook to a file
-            workbook.save('/path/to/directory/SOPRuns.xlsx')
-            
-# Specify the element tag, attribute name, attribute name 2, and target value
-element_tag = 'ClusterRunMetadata'
-attribute_name = 'Fragment_FilePath'
-attribute_name2 = 'Run_Folder_Name'
-target_value = 'STANDARD'
-
-
-
-
-        
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+pattern = '\\OMNIOME.LOCAL\Shares\Shared\systems integration\__SystemsIntegration_Team_Members\_JW\Onso\OCG\Flow Cell Cluster Density Investigation\OCGs\OCG1\10-1.24 run folder\20231002_FC1004309-BCC_OCG01_158\Logs\FC1004309-BCC_metadata.xml'
 
